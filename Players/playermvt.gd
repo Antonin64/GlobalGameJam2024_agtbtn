552ece0f5ccbdec6a172944f4222bac0b36ccf53
CB4fr2 <baptiste.camerlynck@epitech.eu>
2024-01-26 23:10:42 +0100

    Added jump and dep with controller

--- Players/playermvt.gd
+++ Players/playermvt.gd
@@ -4,22 +4,28 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var move_dir = 0 #only on x
+var shoot_dir = Vector2(0, 0) #on 360°
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
+	shoot_dir.x = -Input.get_action_strength("aim_left") + Input.get_action_strength("aim_right")
+	shoot_dir.y = +Input.get_action_strength("aim_down") - Input.get_action_strength("aim_up")
+	move_dir = -Input.get_action_strength("move_left") + Input.get_action_strength("move_right")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump"):# and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = move_dir
 	if direction:
 		velocity.x = direction * SPEED
 	else:
