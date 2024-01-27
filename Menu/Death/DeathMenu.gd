c8651baca28cb01649b5671a5b0b1e4ec8ad2ba7
totosaure <antonin.veau@epitech.eu>
2024-01-27 20:13:32 +0100

    Menu

--- Menu/Death/DeathMenu.gd
+++ Menu/Death/DeathMenu.gd
@@ -6,7 +6,7 @@ var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CanvasLayer/AspectRatioContainer/ButtonsContainer/RetryBtn.grab_focus()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Menu/menu.gd
+++ Menu/menu.gd
@@ -3,6 +3,9 @@ extends Control
 var videos = ["res://Menu/Loading/Bacon.tscn", "res://Menu/Loading/Chips.tscn", "res://Menu/Loading/Dorito.tscn"]
 var rng = RandomNumberGenerator.new()
 
+func _ready():
+	$Menu/ButtonsContainer/Start.grab_focus()
+
 func _on_start_pressed():
 	var randint = rng.randi_range(0, videos.size() - 1)
 	print(randint)
