1139814a166cecc2735797c45c0a7f4b0e1be4d3
totosaure <antonin.veau@epitech.eu>
2024-01-28 11:26:06 +0100

    Dragon qui regarde dans le bon sens

--- Enemies/Enemy1/Enemy1.gd
+++ Enemies/Enemy1/Enemy1.gd
@@ -7,14 +7,24 @@ extends CharacterBody2D
 @export var speed = 0
 
 var player = null
+var last_position = 0
+
+func get_animation_for(direction: Vector2):
+	if direction.x < 0:
+		scale.x = -1
+	if direction.x > 0:
+		scale.x = 1
 
 func _ready():
 	player = get_node("../../../../../CharacterBody2D")
 	state_machine.start("fly", true)
+	last_position = global_position
 
 func _process(delta):
 	if state_machine.get_current_node() == "End":
 		queue_free()
+	get_animation_for(last_position - global_position)
+	last_position = global_position
 
 func _physics_process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed * delta)
