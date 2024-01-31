e97c355319f2e7a2e1af2f0710131d5bed57610e
totosaure <antonin.veau@epitech.eu>
2024-01-31 22:41:28 +0100

    fullscreen bug

--- Global.gd
+++ Global.gd
@@ -6,6 +6,9 @@ var scene = null
 var username = ""
 var videos = ["res://Menu/Loading/Bacon.tscn", "res://Menu/Loading/Chips.tscn", "res://Menu/Loading/Dorito.tscn", "res://Menu/Loading/Citron.tscn", "res://Menu/Loading/Micro-onde.tscn", "res://Menu/Loading/Scie-sauteuse.tscn", "res://Menu/Loading/Tondeuse.tscn", "res://Menu/Loading/Tronconneuse.tscn"]
 
+func _ready():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
 func launch_next_map():
 	var randint = rng.randi_range(0, videos.size() - 1)
 	get_tree().change_scene_to_file.bind(videos[randint]).call_deferred()
