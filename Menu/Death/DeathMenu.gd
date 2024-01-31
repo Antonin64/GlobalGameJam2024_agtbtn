fbaed95bcc1dc20328f9e6a8ca92ee64361a2dd3
CB4fr2 <baptiste.camerlynck@epitech.eu>
2024-01-31 15:32:49 +0100

    New link for sheet

--- Menu/Death/DeathMenu.gd
+++ Menu/Death/DeathMenu.gd
@@ -3,11 +3,9 @@ extends Control
 var videos = ["res://Menu/Loading/Bacon.tscn", "res://Menu/Loading/Chips.tscn", "res://Menu/Loading/Dorito.tscn"]
 var rng = RandomNumberGenerator.new()
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$CanvasLayer/AspectRatioContainer/ButtonsContainer/RetryBtn.grab_focus()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,7 +14,6 @@ func _process(delta):
 func _on_retry_btn_pressed():
 	var randint = rng.randi_range(0, videos.size() - 1)
 	get_tree().change_scene_to_file(videos[randint])
-
 
 func _on_quit_btn_pressed():
 	get_tree().quit()
